33f8069abd46e24cd4740474cc9f27809255fb7a
Oleksandr Khomin <okhomin@e2r6p10.unit.ua>
2019-11-06 15:29:20 +0200

    end

--- mx_push_front.c
+++
@@ -1,9 +0,0 @@
-#include "libmx.h"
-
-t_list *mx_create_node(void *data);
-
-void mx_push_front(t_list **list, void *data) {
-    t_list *front = mx_create_node(data);
-    front->next = *list;
-    *list = front;
-}
