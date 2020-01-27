cb527f675f5dfc65d88f1db9ffb2c2347b95f387
okhomin <okhomin@e2r6p4.unit.ua>
2020-01-27 21:18:57 +0200

    fixed put_map

--- src/mx_put_map.c
+++ src/mx_put_map.c
@@ -1,30 +1,32 @@
 #include "libmx.h"
 
-static void put_value(t_node *node, char *key, void *value);
+static void put_value(t_node **node, char *key, void *value);
 
 void mx_put_map(t_map **map, char *key, void *value) {
     int index = mx_hash(key) % (*map)->size;
 
-    put_value((*map)->nodes[index], key, value);
+    printf("index = %d\n", index);
+    put_value(&(*map)->nodes[index], key, value);
 }
 
-static void put_value(t_node *node, char *key, void *value) {
+static void put_value(t_node **node, char *key, void *value) {
     t_node *new_node = NULL;
+    t_node *last = NULL;
 
-    while (node) {
-        if (!mx_strcmp(node->key, key)) {
-            free(node->data);
-            node->data = value;
-            break;
+    for (t_node *tmp = *node; tmp; tmp = tmp->next) {
+        if (!mx_strcmp(tmp->key, key)) {
+            free(tmp->data);
+            tmp->data = value;
+            return;
         }
-        if (!node->next) {
-            new_node = malloc(sizeof(t_node));
-            new_node->data = value;
-            new_node->key = key;
-            new_node->next = NULL;
-            node->next = new_node;
-            break;
-        }
-        node = node->next;
+        last = tmp;
     }
+    new_node = malloc(sizeof(t_node));
+    new_node->data = value;
+    new_node->key = key;
+    new_node->next = NULL;
+    if (last)
+        last->next = new_node;
+    else
+        *node = new_node;
 }
